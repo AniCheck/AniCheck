235e1c1e54c4f19f31e25d80785c6578b83b16c6
Arminas Sabonis <brayarminas@gmail.com>
2025-06-05 19:17:01 +0300

    Fix

--- src/db/model/MangaEntry.ts
+++ src/db/model/MangaEntry.ts
@@ -1,11 +1,18 @@
-import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+import { BaseEntity, Column, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
 import UserProgress from "./UserProgress"
+import User from "./User"
 
 
 @Entity("MangaEntry")
 export default class MangaEntry extends BaseEntity{
     @PrimaryGeneratedColumn()
     MangaEntryID: number
+
+    @Column()
+    MangaID: number
+
+    @ManyToOne(() => User, (User) => User.MangaEntries, {})
+    User: User
 
     @Column()
     Rating: number
